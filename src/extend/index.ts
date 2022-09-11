75a0399c09fffb8cacc9d33fb76cef4d1cdf354f
Y-Hui <y-hui.mail@foxmail.com>
2022-09-11 11:14:28 +0800

    🎈 perf: 删除 derived

--- src/extend/index.ts
+++ src/extend/index.ts
@@ -1,5 +1,4 @@
 export { default as computed } from './computed'
-export { default as derived } from './derived'
 export { default as selectorHook } from './react/selector-hook'
 export { default as stateHook } from './react/state-hook'
 export { default as valueHook } from './react/value-hook'
